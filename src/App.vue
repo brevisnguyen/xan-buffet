dbf1e0fbd16ebfa45b01fb1a68f34d77b2479e52
Brevis Nguyen <brevisnguyen@gmail.com>
2025-02-13 16:07:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref, computed } from "vue";
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+</script>
+
+<template>
+    <Home></Home>
+</template>
