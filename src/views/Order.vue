d8ceb12f08adc360c69031177cbc092c22965225
Brevis Nguyen <brevisnguyen@gmail.com>
2025-05-10 00:12:26 +0800

    update order

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,24 +1,16 @@
 <script setup>
 import { ref } from "vue";
 import Header from "./partials/Header.vue";
+import menu from "../services/menu.json";
+
+const today = new Date().getDay();
+const title = menu[today].title;
+const items = menu[today].items;
 
 const activeStep = ref(1);
 const activateCallback = (step) => {
     activeStep.value = step;
 };
-const name = ref();
-const email = ref();
-const password = ref();
-const option1 = ref(false);
-const option2 = ref(false);
-const option3 = ref(false);
-const option4 = ref(false);
-const option5 = ref(false);
-const option6 = ref(false);
-const option7 = ref(false);
-const option8 = ref(false);
-const option9 = ref(false);
-const option10 = ref(false);
 </script>
 
 <template>
@@ -95,11 +87,26 @@ const option10 = ref(false);
             <StepPanels>
                 <StepPanel v-slot="{ activateCallback }" :value="1">
                     <div class="flex flex-col gap-2 mx-auto">
-                        <div class="text-center mt-2 md:my-4 text-xl font-semibold">Chọn món</div>
                         <Message size="small" severity="info" closable>
                             Quý khách vui lòng chọn 6 món, mỗi suất gồm 1 hộp cơm, 1 hộp đồ ăn, canh và dụng cụ ăn uống.
                             Nếu có yêu cầu đặc biệt, vui lòng ghi chú trong phần ghi chú.
                         </Message>
+                        <div class="text-center my-2 md:my-4 text-xl font-semibold">Chọn món</div>
+                        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2.5 md:gap-4">
+                            <div v-for="(item, index) in items" :key="index">
+                                <Card class="overflow-hidden">
+                                    <template #header>
+                                        <img :src="item.image" />
+                                    </template>
+                                    <template #title>{{ item.title }}</template>
+                                    <template #footer>
+                                        <div class="flex">
+                                            <Button label="Chọn" class="w-full" />
+                                        </div>
+                                    </template>
+                                </Card>
+                            </div>
+                        </div>
                     </div>
                     <div class="pt-6 justify-end hidden md:flex">
                         <Button label="Tiếp" icon="pi pi-arrow-right" iconPos="right" @click="activateCallback(2)" />
@@ -107,19 +114,8 @@ const option10 = ref(false);
                 </StepPanel>
                 <StepPanel v-slot="{ activateCallback }" :value="2">
                     <div class="flex flex-col gap-2 mx-auto" style="min-height: 16rem; max-width: 24rem">
-                        <div class="text-center mt-4 mb-4 text-xl font-semibold">Choose your interests</div>
-                        <div class="flex flex-wrap justify-center gap-4">
-                            <ToggleButton v-model="option1" onLabel="Nature" offLabel="Nature" />
-                            <ToggleButton v-model="option2" onLabel="Art" offLabel="Art" />
-                            <ToggleButton v-model="option3" onLabel="Music" offLabel="Music" />
-                            <ToggleButton v-model="option4" onLabel="Design" offLabel="Design" />
-                            <ToggleButton v-model="option5" onLabel="Photography" offLabel="Photography" />
-                            <ToggleButton v-model="option6" onLabel="Movies" offLabel="Movies" />
-                            <ToggleButton v-model="option7" onLabel="Sports" offLabel="Sports" />
-                            <ToggleButton v-model="option8" onLabel="Gaming" offLabel="Gaming" />
-                            <ToggleButton v-model="option9" onLabel="Traveling" offLabel="Traveling" />
-                            <ToggleButton v-model="option10" onLabel="Dancing" offLabel="Dancing" />
-                        </div>
+                        <div class="text-center md:my-4 text-xl font-semibold">Choose your interests</div>
+                        <div class="flex flex-wrap justify-center gap-4"></div>
                     </div>
                     <div class="flex pt-6 justify-between">
                         <Button
@@ -134,9 +130,7 @@ const option10 = ref(false);
                 <StepPanel v-slot="{ activateCallback }" :value="3">
                     <div class="flex flex-col gap-2 mx-auto" style="min-height: 16rem; max-width: 24rem">
                         <div class="text-center mt-4 mb-4 text-xl font-semibold">Account created successfully</div>
-                        <div class="flex justify-center">
-                            <img alt="logo" src="https://primefaces.org/cdn/primevue/images/stepper/content.svg" />
-                        </div>
+                        <div class="flex justify-center"></div>
                     </div>
                     <div class="flex pt-6 justify-between">
                         <Button
@@ -164,6 +158,7 @@ const option10 = ref(false);
                 severity="secondary"
                 label="Quay lại"
                 icon="pi pi-arrow-left"
+                size="small"
                 @click="activateCallback(activeStep - 1)"
             />
             <Button
@@ -171,6 +166,7 @@ const option10 = ref(false);
                 :severity="activeStep < 3 ? 'primary' : 'success'"
                 :icon="activeStep < 3 ? 'pi pi-arrow-right' : 'pi pi-check'"
                 iconPos="right"
+                size="small"
                 @click="activateCallback(activeStep + 1)"
             />
         </div>
