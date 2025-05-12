53f8068fe0becc48afb99154b33de73938562f74
Brevis Nguyen <brevisnguyen@gmail.com>
2025-05-13 00:09:40 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Order from '../views/Order.vue'
 import OrderStatus from '../views/OrderStatus.vue'
+import TabTest from '../views/TabTest.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/order', name: 'Order', component: Order },
-  { path: '/status', name: 'Status', component: OrderStatus }
+  { path: '/status', name: 'Status', component: OrderStatus },
+  { path: '/tab', name: 'TabTest', component: TabTest }
 ]
 
 const router = createRouter({
